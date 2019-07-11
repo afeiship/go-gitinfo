271db4508f541c60287c3264f3d9fae72454c6b4
afeiship <1290657123@qq.com>
2019-07-11 23:16:03 +0800

    add: npm to index

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-var prelogin = require('./lib/prelogin');
-var login = require('./lib/login');
-var weibosso = require('./lib/weibo-sso');
+// next packages:
+require('next-js-core2');
+require("next-node-base64");
+require('next-random');
+require('next-param');
+require('next-json');
 
 
-module.exports = function (inOptions) {
-  return new Promise(function (resolve, reject) {
-    prelogin(inOptions).then(function (res1) {
-      login(inOptions, res1).then(function (res2) {
-        weibosso(inOptions, res2).then(function (res3) {
-          resolve(res3);
-        });
-      }, function (error) {
-        reject(error);
-      });
+var prelogin = require("./lib/prelogin");
+var login = require("./lib/login");
+var weibosso = require("./lib/weibo-sso");
+
+module.exports = function(inOptions) {
+  return new Promise(function(resolve, reject) {
+    prelogin(inOptions).then(function(res1) {
+      login(inOptions, res1).then(
+        function(res2) {
+          weibosso(inOptions, res2).then(function(res3) {
+            resolve(res3);
+          });
+        },
+        function(error) {
+          reject(error);
+        }
+      );
     });
   });
 };
